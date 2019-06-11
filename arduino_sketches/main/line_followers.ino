50f5ec90d3baecc6f7733e4b681ccee44f953424
kullken <karl.viktor.kull@gmail.com>
2019-06-11 15:17:29 +0200

    Working custom messages.

--- arduino_sketches/main/line_followers.ino
+++ arduino_sketches/main/line_followers.ino
@@ -2,22 +2,11 @@
 #define lineFollowerMiddlePin 3
 #define lineFollowerRightPin 4
 
-volatile bool lineFollowerLeftValue;
-volatile bool lineFollowerMiddleValue;
-volatile bool lineFollowerRightValue;
 
 void lineFollowerSetup() {
   pinMode(lineFollowerLeftPin, INPUT);
   pinMode(lineFollowerMiddlePin, INPUT);
   pinMode(lineFollowerRightPin, INPUT);
-
-//  enableInterrupt(lineFollowerLeftPin, callbackLineFollowerLeft, CHANGE);
-//  enableInterrupt(lineFollowerMiddlePin, callbackLineFollowerMiddle, CHANGE);
-//  enableInterrupt(lineFollowerRightPin, callbackLineFollowerRight, CHANGE);
-//
-//  lineFollowerLeftValue = digitalRead(lineFollowerLeftPin);
-//  lineFollowerMiddleValue = digitalRead(lineFollowerMiddlePin);
-//  lineFollowerRightValue = digitalRead(lineFollowerRightPin);
   
   //lineFollowerMsg.header.frame_id = "line_followers";
 }
@@ -29,17 +18,5 @@ void lineFollowerUpdate() {
   lineFollowerMsg.middle = digitalRead(lineFollowerMiddlePin);
   lineFollowerMsg.right = digitalRead(lineFollowerRightPin);
 
-  //lineFollowerPub.publish(&lineFollowerMsg);
-}
-
-void callbackLineFollowerLeft() {
-  lineFollowerLeftValue = digitalRead(lineFollowerLeftPin);
-}
-
-void callbackLineFollowerMiddle() {
-  lineFollowerMiddleValue = digitalRead(lineFollowerMiddlePin);
-}
-
-void callbackLineFollowerRight() {
-  lineFollowerRightValue = digitalRead(lineFollowerRightPin);
+  lineFollowerPub.publish(&lineFollowerMsg);
 }
--- arduino_sketches/main/main.ino
+++ arduino_sketches/main/main.ino
@@ -1,57 +1,41 @@
+//Confirmed working with Arduino IDE 1.8.9 
+//Dependencies: EnableInterrupt, Rosserial Arduino Library
+//Custom ros packages needs to be processed, guide on git.
+
 #include <EnableInterrupt.h>
 
 #include <ros.h>
 #include <ros/time.h>
 #include <std_msgs/String.h>
-#include <std_msgs/Bool.h>
 #include <test_pkg/TripleBoolean.h>
-#include <test_pkg/TestBoolean.h>
-#include <sensor_msgs/Temperature.h>
 
 ros::NodeHandle nh;
 
 std_msgs::String strMsg;
 ros::Publisher chatterPub("chatter", &strMsg);
 
-sensor_msgs::Temperature TESTMESSAGE;
-ros::Publisher TESTPUBLISHER("TEST", &TESTMESSAGE);
-
 test_pkg::TripleBoolean lineFollowerMsg;
-//ros::Publisher lineFollowerPub("line_followers", &lineFollowerMsg);
-//test_pkg::TestBoolean lineFollowerTestMsg;
-//ros::Publisher lineFollowerTestPub("line_follower_test", &lineFollowerTestMsg);
+ros::Publisher lineFollowerPub("line_followers", &lineFollowerMsg);
 
 char hello[13] = "hello world!";
 
 void setup()
 {
   nh.initNode();
-  while (!nh.connected()) {nh.spinOnce();};
-
-  nh.advertise(TESTPUBLISHER);
   
-  //nh.advertise(chatterPub);
-  //nh.advertise(lineFollowerPub);
-  //nh.advertise(lineFollowerTestPub);
+  nh.advertise(chatterPub);
+  nh.advertise(lineFollowerPub);
 
-  //lineFollowerSetup();
+  lineFollowerSetup();
 
-  //lineFollowerTestMsg.data = false;
 }
 
 void loop()
 {
   strMsg.data = hello;
   chatterPub.publish(&strMsg);
-
-  TESTMESSAGE.temperature = 1;
-
-  TESTPUBLISHER.publish(&TESTMESSAGE);
   
-  //lineFollowerUpdate();
-
-  //lineFollowerTestMsg.data = !lineFollowerTestMsg.data;
-  //lineFollowerTestPub.publish(&lineFollowerTestMsg);
+  lineFollowerUpdate();
   
   nh.spinOnce();
   delay(1000);
