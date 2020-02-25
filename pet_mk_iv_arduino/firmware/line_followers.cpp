951f322d0ffcc3e6c1564a28b53edfc422316b5e
Viktor <viktorku@kth.se>
2020-02-25 16:02:57 +0100

    Split line followers into .h and .cpp files.

---
+++ pet_mk_iv_arduino/firmware/line_followers.cpp
@@ -0,0 +1,36 @@
+#include "line_followers.h"
+
+#include <Arduino.h>
+
+#include "ros.h"
+
+#include "pet_mk_iv_msgs/TripleBoolean.h"
+
+#define lineFollowerLeftPin 2
+#define lineFollowerMiddlePin 3
+#define lineFollowerRightPin 4
+
+extern pet::ros::NodeHandle nh;
+pet_mk_iv_msgs::TripleBoolean lineFollowerMsg;
+ros::Publisher lineFollowerPub("line_followers", &lineFollowerMsg);
+
+void lineFollowerSetup()
+{
+    pinMode(lineFollowerLeftPin, INPUT);
+    pinMode(lineFollowerMiddlePin, INPUT);
+    pinMode(lineFollowerRightPin, INPUT);
+
+    lineFollowerMsg.header.frame_id = "line_followers";
+    nh.advertise(lineFollowerPub);
+}
+
+void lineFollowerUpdate()
+{
+    lineFollowerMsg.header.stamp = nh.now();
+
+    lineFollowerMsg.left = digitalRead(lineFollowerLeftPin);
+    lineFollowerMsg.middle = digitalRead(lineFollowerMiddlePin);
+    lineFollowerMsg.right = digitalRead(lineFollowerRightPin);
+
+    lineFollowerPub.publish(&lineFollowerMsg);
+}
