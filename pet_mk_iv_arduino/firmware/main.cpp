363547e4cbf747e145b04d0c89eb226bef5853a2
Viktor <viktorku@kth.se>
2020-02-25 15:19:22 +0100

    Spelling fix

--- pet_mk_iv_arduino/firmware/main.cpp
+++ pet_mk_iv_arduino/firmware/main.cpp
@@ -9,9 +9,9 @@
 #include "dist_sensors.h"
 #include "chatter.h"
 
-#define ENGINE_CALLBACK_INTERVALL ros::Duration(0, 20000000)      // 20 ms -> 50 Hz
-#define LF_CALLBACK_INTERVALL ros::Duration(0, 10000000)          // 10 ms -> 100 Hz
-#define DIST_SENSOR_CALLBACK_INTERVALL ros::Duration(0, 33000000) //  33 ms -> ~30 Hz
+#define ENGINE_CALLBACK_INTERVAL ros::Duration(0, 20000000)      // 20 ms -> 50 Hz
+#define LF_CALLBACK_INTERVAL ros::Duration(0, 10000000)          // 10 ms -> 100 Hz
+#define DIST_SENSOR_CALLBACK_INTERVAL ros::Duration(0, 33000000) // 33 ms -> ~30 Hz
 
 pet::ros::NodeHandle nh;
 Timer<4> timer(nh);
@@ -29,9 +29,9 @@ void setup()
     distSensorSetup();
     chatterSetup();
 
-    timer.register_callback(enginesUpdate, ENGINE_CALLBACK_INTERVALL);
-    timer.register_callback(lineFollowerUpdate, LF_CALLBACK_INTERVALL);
-    timer.register_callback(distSensorUpdate, DIST_SENSOR_CALLBACK_INTERVALL);
+    timer.register_callback(enginesUpdate, ENGINE_CALLBACK_INTERVAL);
+    timer.register_callback(lineFollowerUpdate, LF_CALLBACK_INTERVAL);
+    timer.register_callback(distSensorUpdate, DIST_SENSOR_CALLBACK_INTERVAL);
     timer.register_callback(chatterUpdate, ros::Duration(0, 500000000));
 
     nh.loginfo("Arduino setup done!");
