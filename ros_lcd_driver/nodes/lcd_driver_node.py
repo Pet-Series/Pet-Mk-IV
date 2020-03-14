56e8ee36d43529d77080a4a4895a4eeebd49f969
SeniorKullken <stefan.kull@gmail.com>
2020-03-14 23:54:53 +0100

    Updated some code/comments "ROS LCD Driver"

--- ros_lcd_driver/nodes/lcd_driver_node.py
+++ ros_lcd_driver/nodes/lcd_driver_node.py
@@ -5,8 +5,22 @@
 # Route msg that is published on ROS-topic "lcd_display/row1" => subscriber LCD-display Row1
 # Route msg that is published on ROS-topic "lcd_display/row2" => subscriber LCD-display Row2
 #
+# $ roscore
 # $ rosrun ros_lcd_driver lcd_driver_node.py
-#
+# $ rostopic list
+#      /lcd_display/row1
+#      /lcd_display/row2
+# $ rostopic info /lcd_display/row1
+#      Type: std_msgs/String
+#      Publishers: None
+#      Subscribers: /lcd_display (http://raspiKull3:39699/)
+# $ rosmsg show std_msgs/String
+#      string data
+# $ rostopic pub /lcd_display/row1 std_msgs/String "data: 'Hello LCD World'"
+# -----Prerequisite------------------------------
+# <launch>
+#    <node pkg="ros_lcd_driver" type="lcd_driver_node.py" name="lcd_display" output="screen"/>
+# </launch>
 # -----Prerequisite------------------------------
 # Hardware: Display: 1602 16x2 Character LCD Backlight
 # Hardware: I2C interface: PC8574T
@@ -33,7 +47,7 @@ from rpi_lcd import LCD
 class LCDDisplay(object): # A ROS-subscriber node...
 
     def __init__(self):
-        rospy.init_node("lcd_display")
+        rospy.init_node("lcd_display")  # The same name as in 
         self.update_rate = rospy.Rate(1) #1Hz
         
         #Initiate the SSD1306 LCD-display with a PC8574T piggy back I2C-interface
@@ -43,11 +57,11 @@ class LCDDisplay(object): # A ROS-subscriber node...
         self.row1_string = "Init.LCD row1" # Initial text on LCD Display
         self.row2_string = "Init.LCD row2" #     ---- " -----
         
-        # Subscribers
+        # Subscribers (Topucs that is going to be consumed by this node)
         self.row1_sub = rospy.Subscriber("~row1", String, self.row1_cb)
         self.row2_sub = rospy.Subscriber("~row2", String, self.row2_cb)
   
-        # Publishers
+        # Publishers (advertise topics that is going to be published by this module)
         # n/a
 
     def run(self):
@@ -58,8 +72,8 @@ class LCDDisplay(object): # A ROS-subscriber node...
             self.lcd.text(self.row1_string, 1, 'center')
             self.lcd.text(self.row2_string, 2, 'center')
             # Debug text to terminal
-            rospy.loginfo(rospy.get_caller_id() + "LCD-driver Row1=" + self.row1_string ) 
-            rospy.loginfo(rospy.get_caller_id() + "LCD-driver Row2=" + self.row2_string )
+            rospy.loginfo(rospy.get_caller_id() + rospy.get_name() + "/Row1=\"" + self.row1_string + "\"" ) 
+            rospy.loginfo(rospy.get_caller_id() + rospy.get_name() + "/Row2=\"" + self.row2_string + "\"" ) 
             # Put myself into sleep...Zzzz
             self.update_rate.sleep()
             
--- ros_lcd_driver/nodes/test_publisher.py
+++ ros_lcd_driver/nodes/test_publisher.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 # Github @SeniorKullken 2020-03-11
 #
-# Publish current date-time on ROS-topic "lcd_display/row1"
-# Publish how many seconds since start on ROS-topic "lcd_display/row2"
+# On ROS-topic "lcd_display/row1" - Publish current date-time on    ...example "11 Mar 19:20:36"
+# on ROS-topic "lcd_display/row2" - Publish how seconds since start ...example  "Run 56.00 Sec"
 #
 # $ rosrun ros_lcd_driver lcd_driver_node.py
 #
 # -----Prerequisite------------------------------
 # Operating system: Linux/Raspian Buster (based on Debian 10)
-# Middleware: ROS 1.0 Melodic
-# Python 2
+# Middleware: ROS 1.1x Melodic
+# 
+# $ rostopic list
+#      /lcd_display/row1
+#      /lcd_display/row2
 
 import rospy
 import datetime
@@ -32,14 +35,14 @@ class LCDPublisher(object): # A ROS-publisher
     def run(self):
         while not rospy.is_shutdown():
             # Create content to be published
-            self.time_now  = datetime.datetime.now().strftime("%d %b %H:%M:%S") # rospy.get_time()
+            self.time_now  = datetime.datetime.now().strftime("%d %b %H:%M:%S") # Why  not using rospy.get_time() ?
             self.time_past = rospy.get_time() - self.start_time
             # Format the content as strings before published
-            self.row1_str = "%s"   % self.time_now  # Syntax "11 Mar 19:20:36"
-            self.row2_str = "Run %.1f sec" % self.time_past # Syntax "56.00" Seconds
+            self.row1_str = "%s"   % self.time_now          # Syntax "11 Mar 19:20:36"
+            self.row2_str = "Run %.1f sec" % self.time_past # Syntax "Run 56.00 Sec"
             # Debug text to terminal            
-            rospy.loginfo(self.row1_str)
-            rospy.loginfo(self.row2_str)
+            rospy.loginfo("lcd_display/row1=\"" + self.row1_str + "\"")
+            rospy.loginfo("lcd_display/row2=\"" + self.row2_str + "\"")
             # Publish!
             self.row1_pub.publish(self.row1_str)
             self.row2_pub.publish(self.row2_str)
