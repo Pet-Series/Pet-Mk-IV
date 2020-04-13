e2693af2b19dcc11a94e7eefe98048d33420765f
SeniorKullken <stefan.kull@gmail.com>
2020-04-13 19:35:09 +0200

    Work in progress

--- pet_mk_iv_arduino/firmware/main.cpp
+++ pet_mk_iv_arduino/firmware/main.cpp
@@ -7,35 +7,49 @@
 #include "timer.h"
 #include "engines.h"
 #include "line_followers.h"
-#include "dist_sensors.h"
+// #include "dist_sensors.h"
 #include "chatter.h"
+
 
 static const ros::Duration kEngineCallbackInterval(0, 20'000'000);      // 20 ms -> 50 Hz
 static const ros::Duration kLfCallbackInterval(0, 10'000'000);          // 10 ms -> 100 Hz
 static const ros::Duration kDistSensorCallbackInterval(0, 33'000'000);  // 33 ms -> ~30 Hz
 
 pet::ros::NodeHandle nh;
-Timer<4> timer(nh);
+Timer<3> timer(nh);
 
 void setup()
 {
     nh.initNode();
-    delay(1);
 
-    nh.loginfo("Arduino starting...");
+    while (!nh.connected())
+    {
+        nh.spinOnce();
+    }
+
+    nh.logwarn("Arduino starting...");
     nh.spinOnce();
 
     enginesSetup();
     lineFollowerSetup();
-    distSensorSetup();
+    // distSensorSetup();
     chatterSetup();
 
-    timer.register_callback(enginesUpdate, kEngineCallbackInterval);
-    timer.register_callback(lineFollowerUpdate, kLfCallbackInterval);
-    timer.register_callback(distSensorUpdate, kDistSensorCallbackInterval);
-    timer.register_callback(chatterUpdate, ros::Duration(0, 500'000'000));
+    const ros::Duration start_delay(100, 0);
+    const ros::Time global_start_time = nh.now() + start_delay;
 
-    nh.loginfo("Arduino setup done!");
+    timer.register_callback(enginesUpdate, kEngineCallbackInterval, global_start_time);
+    timer.register_callback(lineFollowerUpdate, kLfCallbackInterval, global_start_time);
+    // timer.register_callback(distSensorUpdate, kDistSensorCallbackInterval);
+    timer.register_callback(chatterUpdate, ros::Duration(0, 500'000'000), global_start_time);
+
+    while (!nh.connected())
+    {
+        nh.spinOnce();
+    }
+
+    delay(1000);
+    nh.logwarn("Arduino setup done!");
 }
 
 void loop()
--- pet_mk_iv_arduino/firmware/timer.h
+++ pet_mk_iv_arduino/firmware/timer.h
@@ -27,6 +27,7 @@ public:
     Timer(pet::ros::NodeHandle& nh) : m_nh(nh) {}
 
     void register_callback(func_ptr func, ros::Duration interval);
+    void register_callback(func_ptr func, ros::Duration interval, ros::Time start_time);
     void spin_once();    
 
 private:
@@ -49,13 +50,28 @@ void Timer<capacity>::register_callback(func_ptr func, ros::Duration interval)
 }
 
 template<uint8_t capacity>
+void Timer<capacity>::register_callback(func_ptr func, ros::Duration interval, ros::Time start_time) 
+{
+    if (m_size < capacity) 
+    {
+        m_callbacks[m_size] = Callback(func, interval, start_time);
+        ++m_size;
+    }
+    else {
+        // TODO: Raise exception? Error handling? Ignore?
+    }
+}
+
+template<uint8_t capacity>
 inline void Timer<capacity>::spin_once() 
 {
-    ros::Time current_time = m_nh.now();
-    for (uint8_t i = 0; i < m_size; ++i) {
-        Callback& cb = m_callbacks[i];
-        if (cb.next_time < current_time) {
+    const ros::Time current_time = m_nh.now();
+    for (Callback& cb : m_callbacks) 
+    {
+        if (cb.next_time < current_time)
+        {
             cb.func();
+            // cb.next_time = cb.next_time + cb.interval;
             cb.next_time = current_time + cb.interval;
             break;
         }
