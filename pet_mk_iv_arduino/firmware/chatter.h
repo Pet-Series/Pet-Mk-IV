87da67549172cd66e27bd1136a937d7fdfce599e
kullken <karl.viktor.kull@gmail.com>
2020-05-03 14:34:58 +0200

    Remove unused code.

--- pet_mk_iv_arduino/firmware/chatter.h
+++ pet_mk_iv_arduino/firmware/chatter.h
@@ -8,9 +8,6 @@ extern pet::ros::NodeHandle nh;
 std_msgs::String chatterMsg;
 ros::Publisher chatterPub("chatter", &chatterMsg);
 
-//const char chatterData[8] = "chatter";
-
-// void chatterSetup(ros::NodeHandle& nh)
 void chatterSetup()
 {
     chatterMsg.data = "chatter";
@@ -19,21 +16,7 @@ void chatterSetup()
 
 void chatterUpdate()
 {
-    //chatterMsg.data = chatterData;
     chatterPub.publish(&chatterMsg);
 }
-
-// class chatter
-// {
-// private:
-//     ros::NodeHandle m_nh;
-//     ros::Publisher m_pub;
-//     std_msgs::String m_msg{};
-
-// public:
-//     chatter(ros::NodeHandle& nh) : m_nh(nh), {}
-// };
-
-
 
 #endif
