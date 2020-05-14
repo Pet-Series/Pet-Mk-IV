6e043d4f274561ed1ee8e0f99d008a67f3f4e94d
kullken <karl.viktor.kull@gmail.com>
2020-05-14 18:03:26 +0200

    Use simple open-loop controller.

--- pet_mk_iv_path_planner/nodes/controller.py
+++ pet_mk_iv_path_planner/nodes/controller.py
@@ -10,6 +10,10 @@ from pet_mk_iv_msgs.msg import EngineCommand
 
 class Controller(object):
 
+    width = 0.088
+    pwm_vel_ratio = 255 / 0.52  # Velocity measured with PWM=128 -> 0.26 m/s.
+
+
     def __init__(self):
         rospy.init_node("controller")
         self._cmd_rate = rospy.Rate(100)
@@ -26,24 +30,16 @@ class Controller(object):
             if self._vel_msg is not None:
                 msg = EngineCommand()
                 msg.header.stamp = self._vel_msg.header.stamp
-                msg.left_pwm = 128
-                msg.right_pwm = 128
+                
+                linear_vel = self._vel_msg.twist.linear.x
+                angular_vel = self._vel_msg.twist.angular.z
 
-                if self._vel_msg.twist.linear.x > 0:
-                    msg.left_direction = EngineCommand.FORWARD
-                    msg.right_direction = EngineCommand.FORWARD
-                elif self._vel_msg.twist.linear.x < 0:
-                    msg.left_direction = EngineCommand.BACKWARD
-                    msg.right_direction = EngineCommand.BACKWARD
-                elif self._vel_msg.twist.angular.z > 0:
-                    msg.left_direction = EngineCommand.BACKWARD
-                    msg.right_direction = EngineCommand.FORWARD
-                elif self._vel_msg.twist.angular.z < 0:
-                    msg.left_direction = EngineCommand.FORWARD
-                    msg.right_direction = EngineCommand.BACKWARD
-                else:
-                    msg.left_direction = EngineCommand.STOP
-                    msg.right_direction = EngineCommand.STOP
+                left_wheel, right_wheel = self.to_wheel_vel(linear_vel, angular_vel)
+                msg.left_pwm = self.vel_to_pwm(left_wheel)
+                msg.right_pwm = self.vel_to_pwm(right_wheel)
+
+                msg.left_direction = sign(left_wheel)
+                msg.right_direction = sign(right_wheel)
 
                 self._engine_pub.publish(msg)
                 self._vel_msg = None
@@ -52,6 +48,25 @@ class Controller(object):
 
     def _vel_cb(self, msg):
         self._vel_msg = msg
+
+    def to_wheel_vel(self, linear_vel, angular_vel):
+        """Converts desired linear and angular velocity to desired velocity of left and right wheel."""
+        left_wheel = linear_vel - angular_vel * self.width/2
+        right_wheel = linear_vel + angular_vel * self.width/2
+        return left_wheel, right_wheel
+
+    def vel_to_pwm(self, vel):
+        """Converts velocity to PWM value [0,255]. Ignores signedness of given velocity."""
+        return int(min(abs(vel * self.pwm_vel_ratio), 255))
+
+def sign(x):
+    """Returns (+-)1 with same sign as x (or 0 if x == 0)."""
+    if x > 0:
+        return 1
+    elif x < 0:
+        return -1
+    else:
+        return 0
 
 
 if __name__ == "__main__":
