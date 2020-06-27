3054464d30c806748d59f6ba21c392bc8c4341f8
SeniorKullken <stefan.kull@gmail.com>
2020-06-27 15:39:34 +0200

    Refactor+fix of distance sensor code.

--- pet_mk_iv_arduino/firmware/dist_sensors.cpp
+++ pet_mk_iv_arduino/firmware/dist_sensors.cpp
@@ -5,76 +5,97 @@
 #include <Arduino.h>
 #include <NewPing.h>
 
+#include <pet_mk_iv_msgs/DistanceMeasurement.h>
+
 #include "ros.h"
-#include "pet_mk_iv_msgs/DistanceMeasurement.h"
 
-constexpr unsigned int kTriggerPin1     = 14;   // A0  "Left"
-constexpr unsigned int kEchoPin1        = 14;   // A0  "Left"
-constexpr unsigned int kTriggerPin2     = 15;   // A1  "Middle"
-constexpr unsigned int kEchoPin2        = 15;   // A1  "Middle"
-constexpr unsigned int kTriggerPin3     = 16;   // A2  "Right"
-constexpr unsigned int kEchoPin3        = 16;   // A2  "Right"
 
-constexpr unsigned int kSonarNum        = 3;
-constexpr unsigned int kMaxDistance     = 150;  // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
-constexpr unsigned int kPingInterval    = 33;
+constexpr unsigned int kTriggerPinRight  = 14;   // A0  "Right"
+constexpr unsigned int kEchoPinRight     = 14;   // A0  "Right"
+constexpr unsigned int kTriggerPinMid    = 15;   // A1  "Middle"
+constexpr unsigned int kEchoPinMid       = 15;   // A1  "Middle"
+constexpr unsigned int kTriggerPinLeft   = 16;   // A2  "Left"
+constexpr unsigned int kEchoPinLeft      = 16;   // A2  "Left"
+
+constexpr unsigned int kSensorCount      = 3;
+constexpr unsigned int kMaxDistance      = 400;  // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 
 extern pet::ros::NodeHandle nh;
 static pet_mk_iv_msgs::DistanceMeasurement distSensorMsg;
 static ros::Publisher distSensorPub("dist_sensors", &distSensorMsg);
 
-static unsigned long pingTimer[kSonarNum];
+static void echoCheck();
+
+class Ultrasound
+{
+private:
+    NewPing m_sonar;
+public:
+    const char* m_frame_id;
+
+public:
+    Ultrasound(int triggerPin, int echoPin, const char* frame_id)
+        : m_sonar(triggerPin, echoPin, kMaxDistance)
+        , m_frame_id(frame_id)
+    {
+    }
+
+    void startPing()
+    {
+        m_sonar.ping_timer(echoCheck);
+    }
+
+    void stopPing()
+    {
+        m_sonar.timer_stop();
+    }
+
+    bool pingHasReturned()
+    {
+        return m_sonar.check_timer();
+    }
+
+    int getDistance()
+    {
+        return m_sonar.ping_result * 10 / US_ROUNDTRIP_CM;
+    }
+};
+
 static uint8_t currentSensor = 0;
+static Ultrasound sensors[kSensorCount] = {
+    Ultrasound(kTriggerPinRight, kEchoPinRight, "dist_sensor_right"),
+    Ultrasound(kTriggerPinMid, kEchoPinMid, "dist_sensor_mid"),
+    Ultrasound(kTriggerPinLeft, kEchoPinLeft, "dist_sensor_left")
+    };
 
-constexpr char distSensorFrames[kSonarNum][14] = {
-    "dist_sensor_0",
-    "dist_sensor_1",
-    "dist_sensor_2"};
 
-static NewPing sonar[kSonarNum] = {
-    NewPing(kTriggerPin1, kEchoPin1, kMaxDistance),
-    NewPing(kTriggerPin2, kEchoPin2, kMaxDistance),
-    NewPing(kTriggerPin3, kEchoPin3, kMaxDistance)};
+/// WARNING! This function may be called inside an interrupt.
+static volatile int measuredDistance = -1;
+static void echoCheck()
+{
+    if (sensors[currentSensor].pingHasReturned())
+    {
+        measuredDistance = sensors[currentSensor].getDistance();
+    }
+}
 
 void distSensorSetup()
 {
-    pingTimer[0] = millis() + 75;
-    for (uint8_t i = 1; i < kSonarNum; ++i)
-    {
-        pingTimer[i] = pingTimer[i - 1] + kPingInterval;
-    }
+    sensors[currentSensor].startPing();
 
-    distSensorMsg.header.frame_id = "dist_sensor_x";
     nh.advertise(distSensorPub);
 }
 
 void distSensorUpdate()
 {
-    for (uint8_t i = 0; i < kSonarNum; ++i)
-    {
-        if (millis() >= pingTimer[i])
-        {
-            pingTimer[i] += kPingInterval * kSonarNum;
-            sonar[currentSensor].timer_stop();
-            currentSensor = i;
-            sonar[currentSensor].ping_timer(echoCheck);
-        }
-    }
+    sensors[currentSensor].stopPing();
 
-    distSensorPub.publish(&distSensorMsg);
-}
-
-/// WARNING! This function is called inside an interrupt.
-void echoCheck()
-{
-    if (sonar[currentSensor].check_timer())
-        publishResult(currentSensor, sonar[currentSensor].ping_result * 10 / US_ROUNDTRIP_CM);
-}
-
-/// WARNING! This function is called inside an interrupt.
-void publishResult(uint8_t sensor, int16_t dist)
-{
     distSensorMsg.header.stamp = nh.now();
-    distSensorMsg.header.frame_id = distSensorFrames[sensor];
-    distSensorMsg.distance = dist;
+    distSensorMsg.header.frame_id = sensors[currentSensor].m_frame_id;
+    distSensorMsg.distance = measuredDistance;
+    distSensorPub.publish(&distSensorMsg);
+
+    measuredDistance = -1;
+    currentSensor = (currentSensor + 1) % kSensorCount;
+    sensors[currentSensor].startPing();
 }
--- pet_mk_iv_arduino/firmware/dist_sensors.h
+++ pet_mk_iv_arduino/firmware/dist_sensors.h
@@ -1,14 +1,7 @@
 #ifndef _PET_DISTSENSORS_H
 #define _PET_DISTSENSORS_H
 
-#include <Arduino.h>
-
-#include <stdint.h>
-
 void distSensorSetup();
 void distSensorUpdate();
-
-void echoCheck();
-void publishResult(uint8_t sensor, int16_t dist);
 
 #endif
