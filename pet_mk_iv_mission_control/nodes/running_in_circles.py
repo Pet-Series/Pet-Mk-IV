0a49e5e4bd89eb42e59b98e4e603540da17843aa
kullken <karl.viktor.kull@gmail.com>
2020-06-28 20:12:43 +0200

    Add example running in a circle.

---
+++ pet_mk_iv_mission_control/nodes/running_in_circles.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import rospy
+
+from geometry_msgs.msg import TwistStamped
+
+class CircleRunner(object):
+
+    def __init__(self):
+        rospy.init_node("circle_runner")
+        self.cmd_rate = rospy.Rate(50) # Hz
+
+        # Publishers
+        self.vel_pub = rospy.Publisher("cmd_vel", TwistStamped, queue_size=10)
+        self.vel_msg = TwistStamped()
+        self.vel_msg.header.frame_id = "base_link"
+        self.vel_msg.twist.linear.x = 0.2
+        self.vel_msg.twist.angular.z = 4.5
+
+    def run(self):
+        while not rospy.is_shutdown():
+            self.vel_msg.header.stamp = rospy.Time.now()
+                
+            self.vel_pub.publish(self.vel_msg)        
+            self.cmd_rate.sleep()
+
+if __name__ == '__main__':
+    node = CircleRunner()
+    node.run()
