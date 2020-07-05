269ba6b10e991426edfbccde931a334baf171dbe
kullken <karl.viktor.kull@gmail.com>
2020-07-05 22:12:14 +0200

    Stop primary arduino from chattering.

--- pet_mk_iv_arduino/firmware/src/main.cpp
+++ pet_mk_iv_arduino/firmware/src/main.cpp
@@ -9,7 +9,6 @@
 #include "engines.h"
 #include "line_followers.h"
 #include "dist_sensors.h"
-#include "chatter.h"
 
 
 static const ros::Duration kEngineCallbackInterval(0, 20'000'000);      // 20 ms -> 50 Hz
@@ -17,7 +16,7 @@ static const ros::Duration kLfCallbackInterval(0, 10'000'000);          // 10 ms
 static const ros::Duration kDistSensorCallbackInterval(0, 33'000'000);  // 33 ms -> ~30 Hz
 
 pet::ros::NodeHandle nh;
-Timer<4> timer(nh);
+Timer<3> timer(nh);
 
 void setup()
 {
@@ -33,7 +32,6 @@ void setup()
     enginesSetup();
     lineFollowerSetup();
     distSensorSetup();
-    chatterSetup();
 
     // We need to renegotiate topics after setup-calls since new publishers/subscribers is registered.
     nh.negotiateTopics();
@@ -52,7 +50,6 @@ void setup()
     timer.register_callback(enginesUpdate, kEngineCallbackInterval, start_time);
     timer.register_callback(lineFollowerUpdate, kLfCallbackInterval, start_time);
     timer.register_callback(distSensorUpdate, kDistSensorCallbackInterval);
-    timer.register_callback(chatterUpdate, ros::Duration(0, 500'000'000), start_time);
 
     nh.loginfo("Arduino setup done!");
 }
