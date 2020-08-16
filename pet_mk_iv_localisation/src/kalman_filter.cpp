6a43bb16add756f007849f7c3b88ae405f52e7dc
kullken <karl.viktor.kull@gmail.com>
2020-08-16 16:20:46 +0200

    Start on kalman filter prediction step

--- pet_mk_iv_localisation/src/kalman_filter.cpp
+++ pet_mk_iv_localisation/src/kalman_filter.cpp
@@ -2,6 +2,7 @@
 
 #include <ugl/math/vector.h>
 #include <ugl/math/matrix.h>
+#include <ugl/lie_group/rotation2d.h>
 
 namespace pet
 {
@@ -15,7 +16,21 @@ KalmanFilter::KalmanFilter(double theta, const ugl::Vector<2>& position, const u
 
 void KalmanFilter::predict(double dt, const ugl::Vector3& acc, const ugl::Vector3& ang_vel)
 {
-    // TODO: Implement this!
+    const ugl::Vector<2> acc2d{acc.x(), acc.y()};
+    const ugl::lie::Rotation2D R{m_theta};
+
+    // State propagation
+    const double new_theta = m_theta + ang_vel.z() * dt;
+    const ugl::Vector<2> new_pos = m_pos + (R * m_vel * dt) + (R * acc2d * 0.5*dt*dt);
+    const ugl::Vector<2> new_vel = m_vel + acc2d * dt;
+
+    // Error propagation
+    Jacobian<5,5> A = Jacobian<5,5>::Zero(); // df/dX
+    Jacobian<5,3> B = Jacobian<5,3>::Zero(); // df/dv
+
+    m_theta = new_theta;
+    m_pos = new_pos;
+    m_vel = new_vel;
 }
 
 } // namespace pet
