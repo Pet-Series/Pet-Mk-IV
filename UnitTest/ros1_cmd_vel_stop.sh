ed14702cc2844ac023db49fb6140aa2b6dc933e7
Stefan Kull <stefan.kull@gmail.com>
2023-06-12 19:10:20 +0200

    Added rostopic pub cmd_vel bash script

---
+++ UnitTest/ros1_cmd_vel_stop.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# ROS1 - Test bash script
+echo "Stop"
+source ~/ros_ws/devel/setup.bash 
+rostopic pub -1 /cmd_vel geometry_msgs/TwistStamped "header:
+  seq: 0
+  stamp:
+    secs: 0
+    nsecs: 0
+  frame_id: ''
+twist:
+  linear:
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  angular:
+    x: 0.0
+    y: 0.0
+    z: 0.0"
+echo "Done"
